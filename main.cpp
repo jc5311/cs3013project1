99fceb7fc580460df58032c9062a8e8c3a148861
JuanC <jc.psp5@gmail.com>
2018-08-27 22:06:09 -0400

    Modified main.cpp to loop and fill the arg_buff buffer insteadof hard filling it. This way entries greater than 2 arguments can be made.

--- main.cpp
+++ main.cpp
@@ -7,15 +7,26 @@ int main(int argc, char* argv[]){
      /* argc -- number of arguments */
      /* argv -- an array of strings */
 
+	char* arg_buff[argc];
+	int i;
+
 	for (int i = 0; i < argc; i++){
 		printf("argv[%d]: %s\n", i, argv[i]);
 	}
 
 	//create a buffer of arguments to be fed to command to be called
-	char* arg_buff[argc];
-	arg_buff[0] = argv[1];
-	arg_buff[1] = argv[2];
-	arg_buff[2] = NULL;
+	//arg_buff[0] = argv[1];
+	//arg_buff[1] = argv[2];
+	//arg_buff[2] = NULL;
+
+	//note that argc - 1 will be the last element of the array of input args
+	for (i = 0; i < (argc - 1); i++){
+		//here we assign arg_buff[i] with argv[i+1] to capture every element
+		//after the initial program call of ./main
+		arg_buff[i] = argv[i+1];
+	}
+	//null terminate the buffer
+	arg_buff[i] = NULL;
 
 	printf("Running execv...\n");
 	//note that argv[1] is the desired command to be run from the cli
