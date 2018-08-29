0ceb87564d8ad0d205b69f0ed2ad052c3f79d472
JuanC <jc.psp5@gmail.com>
2018-08-29 00:21:03 -0400

    Very crude shell created. Must be modified to request data if only a newline is entered.

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 
 output: main.o
-	g++ main.o -o main
+	g++ main.o -o doit
 
 main.o: main.cpp
 	g++ -c main.cpp
 
 clean:
-	rm *.o main
+	rm *.o doit
--- main.cpp
+++ main.cpp
@@ -12,9 +12,8 @@
 using namespace std;
 
 /* **** FUNCTIONS **** */
-
-//get system stats and output them
 void getStats(){
+	//get system stats and output them
 	struct rusage usage;
 
 	if (getrusage(RUSAGE_SELF, &usage ) < 0){
@@ -24,68 +23,82 @@ void getStats(){
 	//print data
 	cout << "*** System resource statistics ***\n";
 	cout << "User CPU time: " << ((usage.ru_utime.tv_sec * 1000) +
-									(usage.ru_utime.tv_usec / 1000))
-									<< "ms\n";
+		(usage.ru_utime.tv_usec / 1000))
+	<< "ms\n";
 	cout << "System CPU time: " << ((usage.ru_stime.tv_sec * 1000) +
-									(usage.ru_stime.tv_usec / 1000))
-									<< "ms\n";
+		(usage.ru_stime.tv_usec / 1000))
+	<< "ms\n";
 	cout << "Wall clock still needed!!!\n";
 	cout << "Involuntary preemptions: " << usage.ru_nivcsw << "\n";
 	cout << "Voluntary preemptions: " << usage.ru_nvcsw << "\n";
 	cout << "Major page faults: " << usage.ru_majflt << "\n";
 	cout << "Minor page faults: " << usage.ru_minflt << "\n";
 	cout << "Max resident set size used: " << usage.ru_maxrss << "kB\n";
-
-
 }
-
-
 
 /* **** MAIN **** */
 int main(int argc, char* argv[]){
      /* argc -- number of arguments */
      /* argv -- an array of strings */
-
 	char* arg_buff[argc]; //buffer to store input args
 	int i; //counter used in looping through input args
 	int pid;
+	int state; // 0 = no work to do -> shell prompt; 1 = work to do
+	int num_arg;
 
-	//print the inputted cli args
-	for (int i = 0; i < argc; i++){
-		printf("argv[%d]: %s\n", i, argv[i]);
-	}
+	if (argc > 1)
+		state = 1;
 
-	//store the args in a null terminated buffer
-	//note that argc - 1 will be the last element of the array of input args
-	for (i = 0; i < (argc - 1); i++){
-		//here we assign arg_buff[i] with argv[i+1] to capture every element
-		//after the initial program call of ./main
-		arg_buff[i] = argv[i+1];
-	}
-	//null terminate the buffer
-	arg_buff[i] = NULL;
+	while(1){
+		if (state == 1){
+			//if arguments beyond the application call were made
+			//then do work on them
 
-	//perform command execution in a child process (do not replace the parent)
-	if ( (pid = fork()) < 0){
-		//if a problem occured when attempting to fork
-		cerr << "Fork error\n";
-		exit(1);
-	}
-	else if (pid == 0){
-		//child process, run command
-		cout << "Running execv...\n";
-		//note that argv[1] is the desired command to be run from the cli
-		execvp(argv[1], arg_buff);
-		perror("execv");
-	}
-	else{
-		//process must be the parent... do nothing for now.
-	}
+			//print the inputted cli args
+			for (int j = 0; i < argc; i++){
+				printf("argv[%d]: %s\n", i, argv[i]);
+			}
 
-	wait(0);
-	//sleep(5);
-	getStats();
-	printf("Done...\n");
-	return 0;
+			//store the args in a null terminated buffer
+			//note that argc - 1 will be the last element of the array of input args
+			for (i = 0; i < (argc - 1); i++){
+				//here we assign arg_buff[i] with argv[i+1] to capture every element
+				//after the initial program call of ./main
+				arg_buff[i] = argv[i+1];
+			}
+			//null terminate the buffer
+			arg_buff[i] = NULL;
+
+			//perform command execution in a child process (do not replace the parent)
+			if ( (pid = fork()) < 0){
+				//if a problem occured when attempting to fork
+				cerr << "Fork error\n";
+				exit(1);
+			}
+			else if (pid == 0){
+				//child process, run command
+				cout << "Running execv...\n";
+				//note that argv[1] is the desired command to be run from the cli
+				execvp(argv[1], arg_buff);
+				perror("execv");
+			}
+			else{
+				//process must be the parent... do nothing for now.
+			}
+
+			wait(0);
+			//sleep(5);
+			getStats();
+			printf("Done...\n");
+		}
+		else{
+			//if no arguments besides the call were made
+			//begin shell and ask for input
+			cout << "==> ";
+			cin >> num_arg;
+			cout << "You entered: " << num_arg << "\n";
+		}
+	}
+return 1; //should no longer be reached
 }
 
